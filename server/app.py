432bbc7f0bc2ac699fe497b4333483394e723357
zeph254 <ularezephaniah@gmail.com>
2025-02-06 20:54:02 +0300

    routes

--- server/app.py
+++ server/app.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
